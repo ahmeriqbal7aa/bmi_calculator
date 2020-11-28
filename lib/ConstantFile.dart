83f8c4d1d63a6739d20def5743d3e5c6575fb10b
ahmer7aa <ahmer5253@gmail.com>
2020-11-29 00:45:08 +0500

    Update ConstantFile.dart

--- lib/ConstantFile.dart
+++ lib/ConstantFile.dart
@@ -3,13 +3,32 @@ import 'package:flutter/material.dart';
 //TODO Initialize const variables "Active and deActive"
 const activeColor = Color(0xFF1D1E33);
 const deActiveColor = Color(0xFF111328);
-//TODO Initialize const variables "LabelStyle"
+//TODO Initialize const variable "LabelStyle"
 const LabelStyle = TextStyle(
   fontSize: 20.0,
   color: Color(0xFF8D8E98),
 );
-//TODO Initialize const variables "NumberStyle"
+//TODO Initialize const variable "NumberStyle"
 const NumberStyle = TextStyle(
   fontSize: 50.0,
   fontWeight: FontWeight.w900,
+);
+//TODO Initialize const variable "CalculateButtonStyle"
+const CalculateButtonStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+//TODO Initialize "ResultFile" Styles
+const ResultTitleStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const ResultTextStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.green,
+);
+const ResultStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
 );
