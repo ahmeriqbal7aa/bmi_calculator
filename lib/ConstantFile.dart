3ace98cffad8d97f5cfcc1d3a7d05d8d08b23bd3
ahmer7aa <ahmer5253@gmail.com>
2020-11-28 21:34:51 +0500

    Create Separate file for constants

---
+++ lib/ConstantFile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//TODO Initialize const variables "Active and deActive"
+const activeColor = Color(0xFF1D1E33);
+const deActiveColor = Color(0xFF111328);
+//TODO Initialize const variables "LabelStyle"
+const LabelStyle = TextStyle(
+  fontSize: 20.0,
+  color: Color(0xFF8D8E98),
+);
